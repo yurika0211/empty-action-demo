919961caf777b0032f8ce67620d9e2b61f845535
shiokou <shiokou0408@gmail.com>
2025-12-19 23:20:19 +0800

    Java action test

---
+++ MyApp/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+        
+        // 演示方法调用
+        int result = add(3, 5);
+        System.out.println("3 + 5 = " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
